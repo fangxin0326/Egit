e73b847565375560585d2b4ab63546d0f9539895
fangxin <514063663@qq.com>
2019-12-05 10:37:41 +0800

    更新2

--- src/com/atguigu/git/Dogujianfa.java
+++ src/com/atguigu/git/Dogujianfa.java
@@ -6,6 +6,8 @@ public class Dogujianfa {
 		// TODO Auto-generated method stub
 		System.out.println("22222222222");
 		System.out.println("33333333333");
+		System.out.println("rrrrrrrrrrr");
+		System.out.println("ggggggggggg");
 	}
 
 }
